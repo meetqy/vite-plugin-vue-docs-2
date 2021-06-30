14b1a75470a01f1665f9a767926ddf21390334ea
meetqy <meetqy@qq.com>
2021-06-30 17:25:20 +0800

    chore: component to route

---
+++ example/src/components/card/card-a.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+export default {
+name: "card-a",
+  props: {
+    // 这是一个name
+    name: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,52 @@
 import type { Plugin } from "vite";
-// import { transformMain } from "./main";
+import { ViteDevServer } from "vite";
+import glob from "glob";
+import fs from "fs";
+import humps from "humps";
+import MarkdownIt from "markdown-it";
+import { transformMain } from "./main";
+import http from "http";
+
+const md = new MarkdownIt({});
 
 function vueDocs(): Plugin {
   return {
     name: "vite-plugin-vue-docs",
-    configureServer(server) {
-      server.watcher();
+    async configureServer(server: ViteDevServer) {
+      const { watcher, middlewares } = server;
+      const root: string = `${process.cwd()}/src/components`;
+
+      let docs: { [key: string]: string | null } = {};
+
+      glob(`${root}/**/*.vue`, {}, (err, files) => {
+        if (err) throw err;
+        files.map((file) => {
+          const path = file.replace(root, "").replace(".vue", "");
+          docs[humps.decamelize(path, { separator: "-" })] = transformMain(
+            fs.readFileSync(file, "utf-8")
+          );
+        });
+      });
+
+      // 构建路由
+      middlewares.use(`/docs`, (req: http.IncomingMessage, res) => {
+        const result = docs[req.url || ""];
+        if (result) {
+          res.writeHead(200, {
+            "content-type": "text/html;charset=utf8",
+          });
+
+          res.end(md.render(result));
+        } else {
+          res.writeHead(404);
+          res.end(JSON.stringify(Object.keys(docs)));
+        }
+      });
+
+      watcher
+        .on("add", (path) => console.log(`File ${path} has been added`))
+        .on("change", (path) => console.log(`File ${path} has been changed`))
+        .on("unlink", (path) => console.log(`File ${path} has been removed`));
     },
   };
 }
