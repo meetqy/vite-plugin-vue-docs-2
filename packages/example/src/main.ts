1a89fca5c8061303763f1f1b8404be2e01182b31
meetqy <meetqy@qq.com>
2021-07-17 17:15:18 +0800

    build: example router

--- packages/example/src/main.ts
+++ packages/example/src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: "/", component: () => import("./views/index.vue") },
     { path: "/setup", component: () => import("./views/setup.vue") },
