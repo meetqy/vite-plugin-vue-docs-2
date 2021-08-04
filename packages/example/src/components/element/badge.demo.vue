af0ebc02453766c3ce6276c38b6dffec7fd6ac0a
meetqy <meetqy@qq.com>
2021-08-04 11:32:31 +0800

    feat: example add demo

---
+++ packages/example/src/components/element/badge.demo.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <el-badge :value="12" class="item">
+      <el-button size="small">评论</el-button>
+    </el-badge>
+    <el-badge :value="3" class="item">
+      <el-button size="small">回复</el-button>
+    </el-badge>
+    <el-badge :value="1" class="item" type="primary">
+      <el-button size="small">评论</el-button>
+    </el-badge>
+    <el-badge :value="2" class="item" type="warning">
+      <el-button size="small">回复</el-button>
+    </el-badge>
+  </div>
+
+  <div style="margin-top: 20px">
+    <h3>最大值</h3>
+    <el-badge :value="200" :max="99" class="item">
+      <el-button size="small">评论</el-button>
+    </el-badge>
+    <el-badge :value="100" :max="10" class="item">
+      <el-button size="small">回复</el-button>
+    </el-badge>
+  </div>
+
+  <div style="margin-top: 20px">
+    <h3>自定义内容</h3>
+    <el-badge value="new" class="item">
+      <el-button size="small">评论</el-button>
+    </el-badge>
+    <el-badge value="hot" class="item">
+      <el-button size="small">回复</el-button>
+    </el-badge>
+  </div>
+</template>
+
+<script>
+import ElButton from "./button.vue";
+import ElBadge from "./badge.vue";
+export default {
+  name: "badge.demo",
+  components: { ElButton, ElBadge },
+};
+</script>
+
+<style scoped>
+.el-badge {
+  margin-right: 30px;
+}
+</style>
---
+++ packages/example/src/components/element/progress.demo.vue
@@ -0,0 +1,106 @@
+<template>
+  <div style="width: 350px">
+    <el-progress :percentage="50"></el-progress>
+    <el-progress :percentage="100" :format="format"></el-progress>
+    <el-progress :percentage="100" status="success"></el-progress>
+    <el-progress :percentage="100" status="warning"></el-progress>
+    <el-progress :percentage="50" status="exception"></el-progress>
+  </div>
+
+  <div style="margin-top: 20px; width: 350px">
+    <h3>百分比内显</h3>
+    <el-progress
+      :text-inside="true"
+      :stroke-width="26"
+      :percentage="70"
+    ></el-progress>
+    <el-progress
+      :text-inside="true"
+      :stroke-width="24"
+      :percentage="100"
+      status="success"
+    ></el-progress>
+    <el-progress
+      :text-inside="true"
+      :stroke-width="22"
+      :percentage="80"
+      status="warning"
+    ></el-progress>
+    <el-progress
+      :text-inside="true"
+      :stroke-width="20"
+      :percentage="50"
+      status="exception"
+    ></el-progress>
+  </div>
+
+  <div style="width: 350px; margin-top: 20px">
+    <h3>自定义内容</h3>
+    <el-progress :percentage="50">
+      <el-button type="text">自定义内容</el-button>
+    </el-progress>
+    <el-progress
+      :text-inside="true"
+      :stroke-width="20"
+      :percentage="50"
+      status="exception"
+    >
+      <span>自定义内容</span>
+    </el-progress>
+    <el-progress type="circle" :percentage="100" status="success">
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+    </el-progress>
+    <el-progress type="dashboard" :percentage="80">
+      <template #default="{ percentage }">
+        <span class="percentage-value">{{ percentage }}%</span>
+        <span class="percentage-label">当前进度</span>
+      </template>
+    </el-progress>
+  </div>
+
+  <div style="width: 350px; margin-top: 20px">
+    <h3>动态进度条</h3>
+    <el-progress :percentage="50" :indeterminate="true"></el-progress>
+    <el-progress
+      :percentage="100"
+      :format="format"
+      :indeterminate="true"
+    ></el-progress>
+    <el-progress
+      :percentage="100"
+      status="success"
+      :indeterminate="true"
+      :duration="5"
+    ></el-progress>
+    <el-progress
+      :percentage="100"
+      status="warning"
+      :indeterminate="true"
+      :duration="1"
+    ></el-progress>
+    <el-progress
+      :percentage="50"
+      status="exception"
+      :indeterminate="true"
+    ></el-progress>
+  </div>
+</template>
+
+<script>
+import ElProgress from "./progress.vue";
+export default {
+  name: "skeleton.demo",
+  components: { ElProgress },
+  methods: {
+    format(percentage) {
+      return percentage === 100 ? "满" : `${percentage}%`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.el-progress {
+  margin-top: 10px;
+}
+</style>
